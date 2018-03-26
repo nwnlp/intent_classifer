20cbd5fd16fb24f96f3053edb70bed988f4d7679
johnny <johnny@johnnydeiMac.local>
2018-03-26 22:47:41 +0800

    add multi channel imp

--- cnn_cls.py
+++ cnn_cls.py
@@ -2,7 +2,13 @@ import os
 import numpy as np
 import cnn_model as model
 import tensorflow as tf
-def load_train_data(dir = 'TREC/train'):
+import time
+from gensim.models import KeyedVectors
+import numpy as np
+import gzip
+import shutil
+
+def load_train_data(dir = 'atis/train'):
     files = os.listdir(dir)
     word2id = {}
     word2id['_PAD_'] = len(word2id)
@@ -10,6 +16,7 @@ def load_train_data(dir = 'TREC/train'):
     intent2id = {}
     data = {}
     max_sent_size = 0
+    sents_size = []
     for file in files:
         for line in open(os.path.join(dir, file),'r').readlines():
             sent = line.strip().split(' ')
@@ -17,10 +24,11 @@ def load_train_data(dir = 'TREC/train'):
                 if word not in word2id:
                     word2id[word] = len(word2id)
             max_sent_size = max(max_sent_size, len(sent))
+            sents_size.append(len(sent))
             data.setdefault(file, [])
             data[file].append(sent)
         intent2id[file] = len(intent2id)
-    return word2id, intent2id, data,max_sent_size
+    return word2id, intent2id, data,max_sent_size,sents_size
 
 def dense_to_one_hot(labels_dense, num_classes=10):
     """Convert class labels from scalars to one-hot vectors."""
@@ -29,6 +37,7 @@ def dense_to_one_hot(labels_dense, num_classes=10):
 def vectorize_data(word2id, intent2id, raw_data, vec_size):
     y = []
     x = []
+    text = []
     for intent, sent_list in raw_data.items():
         if intent not in intent2id:
             continue
@@ -41,13 +50,24 @@ def vectorize_data(word2id, intent2id, raw_data, vec_size):
                 else:
                     tmp.append(word2id['_UNKNOW_'])
             if len(tmp) > vec_size:
-                tmp = tmp[vec_size:]
+                tmp = tmp[:vec_size]
                 x.append(tmp)
             elif len(tmp) <= vec_size:
                 tmp.extend([word2id['_PAD_']]*(vec_size-len(tmp)))
                 x.append(np.array(tmp))
             y.append(dense_to_one_hot(intent_id, len(intent2id)))
-    return np.array(x), np.array(y)
+            text.append(sent)
+    return np.array(x), np.array(y), text
+
+def static_channel(word2vec, x_data):
+    result = []
+    for x in x_data:
+        vecs = []
+        for word_id in x:
+            vec = word2vec[word_id]
+            vecs.append(vec)
+        result.append(np.array(vecs))
+    return np.array(result)
 
 def load_dev_test_data(dir):
     data = {}
@@ -59,24 +79,26 @@ def load_dev_test_data(dir):
             data[file].append(sent)
     return data
 
-def batch_iter(X, Y, batch_size=128):
+def batch_iter(X, Y, x_train_st_chn, batch_size=128):
     data_len = len(X)
     num_batch = int((data_len - 1) / batch_size) + 1
 
     indices = np.random.permutation(np.arange(data_len))
     x_shuffle = X[indices]
     y_shuffle = Y[indices]
+    x_st_chn = x_train_st_chn[indices]
 
     for i in range(num_batch):
         start_id = i * batch_size
         end_id = min((i + 1) * batch_size, data_len)
-        yield x_shuffle[start_id:end_id], y_shuffle[start_id:end_id]
+        yield x_shuffle[start_id:end_id], y_shuffle[start_id:end_id], x_st_chn[start_id:end_id]
 
 
-def feed_data(cnn, x_batch, y_batch, keep_prob):
+def feed_data(cnn, x_batch, y_batch, x_st_chn, keep_prob):
     feed_dict = {
-        cnn.input_x: x_batch,
+        cnn.input_channel1_x: x_batch,
         cnn.input_y: y_batch,
+        cnn.input_channel2_x: x_st_chn,
         cnn.keep_prob: keep_prob
     }
     return feed_dict
@@ -87,17 +109,61 @@ def mean_normalize(features):
     mean_value = features.mean()
     return (features - mean_value) / std_value
 
+def dbug_data(X, Y, id2intent, file):
+    x_str = []
+    for i,x in enumerate(X):
+        y = Y[i]
+        intent = id2intent[y.argmax()]
+        x = [str(ele) for ele in x]
+        x_str.append(intent+':'+' '.join(x))
+    result = '\n'.join(x_str)
+    open(file, 'w').write(result)
+
+def load_word2vec(word2id):
+    start = time.time()
+    model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300-SLIM.bin.gz', binary=True)
+    print('Finished loading slim model %.1f sec' % ((time.time() - start)))
+    print('word2vec: %d' % len(model.vocab))
+    word2vec = {}
+    for word,id in word2id.items():
+        if word in model.vocab:
+            word2vec[id] = model.wv[word]
+        else:
+            word2vec[id] = np.random.normal(0,0.1,300)
+    return word2vec
+
 def train(argv=None):
 
-    word2id, intent2id, raw_train_data, vec_size = load_train_data()
-    x_train, y_train = vectorize_data(word2id, intent2id, raw_train_data, vec_size)
-    raw_dev_data = load_dev_test_data('TREC/dev')
-    x_dev, y_dev = vectorize_data(word2id, intent2id, raw_dev_data, vec_size)
-    raw_test_data = load_dev_test_data('TREC/test')
-    x_test, y_test = vectorize_data(word2id, intent2id, raw_test_data, vec_size)
+    word2id, intent2id, raw_train_data, vec_size, sents_size = load_train_data()
+    word2vec = load_word2vec(word2id)
+
+    '''sent_size_cnt = [0]*(vec_size+1)
+    for sent_size in sents_size:
+        sent_size_cnt[sent_size] += 1
+    print(sent_size_cnt)'''
+    vec_size = 20
+    id2intent = {}
+    for intent, id in intent2id.items():
+        id2intent[id] = intent
+    x_train, y_train, text_train = vectorize_data(word2id, intent2id, raw_train_data, vec_size)
+    x_train_st_chn = static_channel(word2vec, x_train)
+
+    raw_dev_data = load_dev_test_data('atis/dev')
+    x_dev, y_dev, text_dev = vectorize_data(word2id, intent2id, raw_dev_data, vec_size)
+    x_dev_st_chn = static_channel(word2vec, x_dev)
+
+    raw_test_data = load_dev_test_data('atis/test')
+    x_test, y_test, text_test = vectorize_data(word2id, intent2id, raw_test_data, vec_size)
+    x_test_st_chn = static_channel(word2vec, x_test)
+
+
     print('vocab size:'+str(len(word2id)))
+    dbug_data(x_train, y_train, id2intent, 'train.data')
+    dbug_data(x_dev, y_dev, id2intent, 'dev.data')
+    dbug_data(x_test, y_test, id2intent, 'test.data')
     config = model.CNNConfig
-    cnn = model.IntentCNN(config, vec_size, len(intent2id), len(word2id))
+    #cnn = model.IntentCNN(config, vec_size, len(intent2id), len(word2id))
+    cnn = model.MultiChnIntentCNN(config, vec_size, len(intent2id), len(word2id))
     session = tf.Session()
     session.run(tf.global_variables_initializer())
     saver = tf.train.Saver(tf.global_variables())
@@ -116,33 +182,54 @@ def train(argv=None):
     valid_writer = tf.summary.FileWriter(tensorboard_valid_dir)
 
     total_batch = 0
+    dev_feed_dict = feed_data(cnn, x_dev,y_dev,x_dev_st_chn,config.dropout_keep_prob)
     for epoch in range(config.num_epochs):
-        #print('Epoch:', epoch + 1)
-        batch_train = batch_iter(x_train, y_train)
-        for x_batch, y_batch in batch_train:
+        print('Epoch:', epoch + 1)
+        batch_train = batch_iter(x_train, y_train,x_train_st_chn)
+        for x_batch, y_batch, x_st_chn in batch_train:
             total_batch += 1
-            feed_dict = feed_data(cnn, x_batch, y_batch, config.dropout_keep_prob)
-            session.run(cnn.optim, feed_dict=feed_dict)
+            train_feed_dict = feed_data(cnn, x_batch, y_batch, x_st_chn, config.dropout_keep_prob)
+            session.run(cnn.optim, feed_dict=train_feed_dict)
             if total_batch % config.print_per_batch == 0:
-                train_loss, train_accuracy = session.run([cnn.loss, cnn.acc], feed_dict=feed_dict)
-                valid_loss, valid_accuracy = session.run([cnn.loss, cnn.acc], feed_dict={cnn.input_x: x_dev,
-                                                                                         cnn.input_y: y_dev,
-                                                                                         cnn.keep_prob: config.dropout_keep_prob})
-                print('Steps:' + str(total_batch))
+                train_loss, train_accuracy = session.run([cnn.loss, cnn.acc], feed_dict=train_feed_dict)
+
+                valid_loss, valid_accuracy, valid_pred, pred_cls = session.run([cnn.loss, cnn.acc, cnn.correct_pred, cnn.y_pred_cls], feed_dict=dev_feed_dict)
+                #print('Steps:' + str(total_batch))
                 print(
                     'train_loss:' + str(train_loss) + ' train accuracy:' + str(train_accuracy) + '\tvalid_loss:' + str(
                         valid_loss) + ' valid accuracy:' + str(valid_accuracy))
+                incorrect = {}
+                all_incorrect_cnt = 0
+                logtmp = []
+                for i, pred in enumerate(valid_pred):
+                    if pred == False:
+                        index = y_dev[i].argmax()
+                        intent = id2intent[index]
+                        incorrect.setdefault(intent, 0)
+                        incorrect[intent] += 1
+                        all_incorrect_cnt += 1
+                        text = text_dev[i]
+                        pred_intent = id2intent[pred_cls[i]]
+                        logtmp.append(' '.join(text) +' '+intent +'->'+pred_intent)
+
+                for intent in incorrect:
+                    incorrect[intent] = incorrect[intent]/all_incorrect_cnt
+                logstr = 'valid incorrect percentage:'
+                for intent, per in incorrect.items():
+                    logstr += intent +' '+str(per)+'\t'
+                #print(logstr)
+                #print('\n'.join(logtmp))
             if total_batch % config.save_tb_per_batch == 0:
-                train_s = session.run(merged_summary, feed_dict=feed_dict)
+                train_s = session.run(merged_summary, feed_dict=train_feed_dict)
                 train_writer.add_summary(train_s, total_batch)
-                valid_s = session.run(merged_summary, feed_dict={cnn.input_x: x_dev, cnn.input_y: y_dev,
-                                                                 cnn.keep_prob: config.dropout_keep_prob})
+                valid_s = session.run(merged_summary, feed_dict=dev_feed_dict)
                 valid_writer.add_summary(valid_s, total_batch)
 
         saver.save(session, checkpoint_path, global_step=epoch)
+    test_feed_dict = feed_data(cnn, x_test,y_test,x_test_st_chn,config.dropout_keep_prob)
+
     test_loss, test_accuracy = session.run([cnn.loss, cnn.acc],
-                                           feed_dict={cnn.input_x: x_test, cnn.input_y: y_test,
-                                                      cnn.keep_prob: config.dropout_keep_prob})
+                                           feed_dict=test_feed_dict)
     print('test_loss:' + str(test_loss) + ' test accuracy:' + str(test_accuracy))
 
 if __name__ == '__main__':
--- cnn_model.py
+++ cnn_model.py
@@ -2,16 +2,88 @@
 import tensorflow as tf
 class CNNConfig(object):
     """CNN配置参数"""
-    embedding_dim = 64      # 词向量维度
-    num_filters = 64        # 卷积核数目
-    filter_sizes = [3,4,5]         # 卷积核尺寸
+    embedding_dim = 300      # 词向量维度
+    num_filters = 128        # 卷积核数目
+    filter_sizes = [2,3,4]         # 卷积核尺寸
     hidden_dim = 32        # 全连接层神经元
     dropout_keep_prob = 0.5 # dropout保留比例
-    learning_rate = 1e-4    # 学习率
-    num_epochs = 500         # 总迭代轮次
+    REGULARIZATION_RATE = 1.0
+    learning_rate = 1e-3    # 学习率
+    num_epochs = 20         # 总迭代轮次
     print_per_batch = 10    # 每多少轮输出一次结果
     save_tb_per_batch = 10
-    batch_size = 128
+    batch_size = 64
+
+class MultiChnIntentCNN(object):
+    def __init__(self, config, seq_length, num_classes, vocab_size):
+        self.config = config
+        self.input_channel1_x = tf.placeholder(tf.int32, [None, seq_length], name='input_channel1_x')
+        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name='input_y')
+        self.input_channel2_x = tf.placeholder(tf.float32, [None, seq_length, self.config.embedding_dim], name='input_channel2_x')
+
+        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+        with tf.device('/cpu:0'):
+            embedding = tf.get_variable('embedding', [vocab_size, self.config.embedding_dim])
+            embedding_inputs = tf.nn.embedding_lookup(embedding, self.input_channel1_x)
+        #print(embedding_inputs.shape)
+        #print(self.input_channel2_x)
+        intput_x = tf.stack([embedding_inputs, self.input_channel2_x], axis = 3)
+        print(intput_x.shape)
+
+        pooled_outputs = []
+        for i, filter_size in enumerate(self.config.filter_sizes):
+            with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # filter_shape = [filter_size, self.config.embedding_dim,self.config.num_filters]
+                '''W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[self.config.num_filters]), name="b")
+                conv = tf.nn.conv2d(
+                    embedding_inputs,
+                    W,
+                    strides=[1, 1, 1,1],
+                    padding='VALID',
+                    name="conv-1"
+                )'''
+                print("conv-maxpool-%s" % filter_size)
+                conv = tf.layers.conv2d(intput_x, self.config.num_filters, [filter_size,self.config.embedding_dim])
+                pooled = tf.reduce_max(conv, reduction_indices=[1])
+                pooled_outputs.append(pooled)
+        num_filters_total = self.config.num_filters * len(self.config.filter_sizes)  # 500*4
+        pooled_reshape = tf.reshape(tf.concat(pooled_outputs, 1), [-1, num_filters_total])
+        pooled_flat = tf.nn.dropout(pooled_reshape, self.keep_prob)
+
+        with tf.name_scope("score"):
+            # 全连接层，后面接dropout以及relu激活
+            #fc = tf.layers.dense(pooled_flat, self.config.hidden_dim, name='fc1')
+            '''weight1 = tf.Variable(tf.truncated_normal([num_filters_total, self.config.hidden_dim], stddev=0.1))
+            biases1 = tf.Variable(tf.constant(0.1, shape=[self.config.hidden_dim]))
+            fc = tf.nn.relu(tf.matmul(pooled_flat, weight1) + biases1)
+            fc = tf.contrib.layers.dropout(fc, self.keep_prob)
+            fc = tf.nn.relu(fc)'''
+            # 分类器
+
+            weight2 = tf.Variable(tf.truncated_normal([num_filters_total, num_classes], stddev=0.1))
+            biases2 = tf.Variable(tf.constant(0.1, shape=[num_classes]))
+            self.logits = tf.nn.relu(tf.matmul(pooled_flat, weight2) + biases2)
+            #self.logits = tf.layers.dense(fc, num_classes,name='fc2')
+
+            #self.logits = tf.contrib.layers.dropout(self.logits, self.keep_prob)
+            self.y_pred_cls = tf.argmax(tf.nn.softmax(self.logits), 1)  # 预测类别
+
+        with tf.name_scope("optimize"):
+            # 损失函数，交叉熵
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.input_y)
+            regularizer = tf.contrib.layers.l2_regularizer(self.config.REGULARIZATION_RATE)
+            regularization = regularizer(weight2)
+            self.loss = tf.reduce_mean(cross_entropy)# + regularization
+
+            # 优化器
+            self.optim = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate).minimize(self.loss)
+
+        with tf.name_scope("accuracy"):
+            # 准确率
+            self.correct_pred = tf.equal(tf.argmax(self.input_y, 1), self.y_pred_cls)
+            self.acc = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))
+
 
 class IntentCNN(object):
     def __init__(self, config, seq_length, num_classes, vocab_size):
@@ -46,25 +118,35 @@ class IntentCNN(object):
 
         with tf.name_scope("score"):
             # 全连接层，后面接dropout以及relu激活
-            fc = tf.layers.dense(pooled_flat, self.config.hidden_dim, name='fc1')
+            #fc = tf.layers.dense(pooled_flat, self.config.hidden_dim, name='fc1')
+            '''weight1 = tf.Variable(tf.truncated_normal([num_filters_total, self.config.hidden_dim], stddev=0.1))
+            biases1 = tf.Variable(tf.constant(0.1, shape=[self.config.hidden_dim]))
+            fc = tf.nn.relu(tf.matmul(pooled_flat, weight1) + biases1)
             fc = tf.contrib.layers.dropout(fc, self.keep_prob)
-            fc = tf.nn.relu(fc)
-            'kernel_regularizer=tf.nn.l2_loss'
+            fc = tf.nn.relu(fc)'''
             # 分类器
-            self.logits = tf.layers.dense(fc, num_classes,name='fc2')
+
+            weight2 = tf.Variable(tf.truncated_normal([num_filters_total, num_classes], stddev=0.1))
+            biases2 = tf.Variable(tf.constant(0.1, shape=[num_classes]))
+            self.logits = tf.nn.relu(tf.matmul(pooled_flat, weight2) + biases2)
+            #self.logits = tf.layers.dense(fc, num_classes,name='fc2')
+
             #self.logits = tf.contrib.layers.dropout(self.logits, self.keep_prob)
             self.y_pred_cls = tf.argmax(tf.nn.softmax(self.logits), 1)  # 预测类别
 
         with tf.name_scope("optimize"):
             # 损失函数，交叉熵
             cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.input_y)
-            self.loss = tf.reduce_mean(cross_entropy)
+            regularizer = tf.contrib.layers.l2_regularizer(self.config.REGULARIZATION_RATE)
+            regularization = regularizer(weight2)
+            self.loss = tf.reduce_mean(cross_entropy)# + regularization
+
             # 优化器
             self.optim = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate).minimize(self.loss)
 
         with tf.name_scope("accuracy"):
             # 准确率
-            correct_pred = tf.equal(tf.argmax(self.input_y, 1), self.y_pred_cls)
-            self.acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+            self.correct_pred = tf.equal(tf.argmax(self.input_y, 1), self.y_pred_cls)
+            self.acc = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))
 
 
